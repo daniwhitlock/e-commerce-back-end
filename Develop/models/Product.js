c64adb03f8ba0ad55e7d51bbf67077f0c0ab2f50
Fire Dragon <daniwhitlock4@gmail.com>
2021-02-14 08:56:59 -0700

    worked on models and routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validating that value is a decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //set default value of 10
+      defaultValue: 10,
+      //validates that the value is numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references the category model's' id
+      references: {
+        model: 'category',
+        //id references column in category table
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+// Category have many Products
+Category.hasMany(Product, {
+  // needs to match products belongTo category since they are talking to each other
+  foreignKey: 'category_id'
+})
+// Products belongsToMany Tag (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //utilizing the foreign key which has a reference to the product table
+  foreignKey: 'product_id'
+});
+// Tag belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
